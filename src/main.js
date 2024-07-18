ec9b3c3ad859044605d21842aae6e8330474445b
Angela-Wehbe <amw001lb@aou.edu.lb>
2024-07-18 19:17:24 +0300

    Finished ltask ya hassan

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
+//3melna express app
 const express = require("express");
 const app = express();
 app.use(express.json());
 
+//wasalna lport mnl .env hek shi
 require("dotenv").config();
 const PORT = process.env.PORT || 3031;
-
+//wasalna lroutes
 const tasks = require('./routes/task');
 
 app.use(tasks);
-
+//bta3tine aya port fet7a bas 2a3ml start
 app.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}`);
 });
